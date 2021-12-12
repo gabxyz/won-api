0638744fc74a55fea7c57e101e4c5b3753e8f519
Gabriel <gabxyzdev@gmail.com>
2021-12-12 16:22:34 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9712c33127192bd6e06ec55a02b81179'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e0c2bf10-93ac-48f6-9edf-6f4541e660f0'
+};
