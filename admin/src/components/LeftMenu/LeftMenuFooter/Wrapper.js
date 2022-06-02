48bc38633f1bd3b46f29b0a9a8205ac2af207f30
Gabriel <gabxyzdev@gmail.com>
2022-06-02 15:13:14 -0300

    Add Wrapper custom styles for the LeftMenu Footer and Header

--- admin/src/components/LeftMenu/LeftMenuFooter/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuFooter/Wrapper.js
@@ -4,7 +4,8 @@ const Wrapper = styled.div`
   ${({ theme }) => css`
     position: absolute;
     width: 100%;
-    background: ${theme.main.colors.won.blueLight};
+    background: ${theme.main.colors.won.blue};
+    border-top: 1px solid rgba(245, 97, 97, 0.3);
     bottom: 0;
     .poweredBy {
       width: 100%;
@@ -14,7 +15,7 @@ const Wrapper = styled.div`
       padding-right: 15px;
       line-height: 3rem;
       background-color: rgba(255, 255, 255, 0.02);
-      font-size: 1rem;
+      font-size: 1.2rem;
       font-weight: 400;
       letter-spacing: 0.05rem;
       vertical-align: middle;
--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -7,6 +7,7 @@ const Wrapper = styled.div`
   ${({ theme }) => css`
     background-color: ${theme.main.colors.won.blue};
     height: ${theme.main.sizes.leftMenu.height};
+    border-bottom: 1px solid rgba(245, 97, 97, 0.3);
 
     .projectName {
       display: block;
